b982da8a176f93a54471810d695d5af80ed695a7
bhnzhang <bhnzhang@gmail.com>
2018-10-25 20:23:00 -0400

    adding startup file and vec2str helper function

---
+++ startup.m
@@ -0,0 +1,8 @@
+% bz startup file
+
+% use software rendering
+opengl software;
+
+% add path to utility functions
+addpath( genpath ('C:\Users\bz\git\my_matlab_utility_functions') );         % plab desktop
+addpath( genpath ('C:\Users\beezy\git\my_matlab_utility_functions') );      % personal laptop
