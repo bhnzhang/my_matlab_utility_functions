9cae80c03cc586cf28da5e72b82316c988ea47f0
bhnzhang <bhnzhang@gmail.com>
2019-03-12 22:04:58 -0400

    function that finds and returns pos of max element of matrix

--- f_get_max_matrix_elem.m
+++ f_get_max_matrix_elem.m
@@ -1,8 +1,8 @@
-function [outputArg1,outputArg2] = f_get_max_matrix_elem( A )
+function [ max_elem, i_row, i_col ] = f_get_max_matrix_elem( A )
 % returns maximum value and indices of largest element of matrix A
 
-[max_elem, indx]    = max(A(:));
-[i_row, i_col]      = ind2sub(size(A),indx);
+[ max_elem, indx ]    = max(A(:));
+[ i_row, i_col ]      = ind2sub(size(A),indx);
 
 end
 
