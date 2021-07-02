e9f9a57c54064d63d556d26c4e7b94e003f0d40a
bz <bhnzhang@gmail.com>
2021-07-02 14:59:59 -0400

    fixed framerate param in save to gif

--- f_save_to_gif.m
+++ f_save_to_gif.m
@@ -1,5 +1,7 @@
 function [] = f_save_to_gif(hfig, framerate, save_gif_filename, i_frame)
 % Save a figure as a frame to a gif
+%
+% framerate: frames per second
 
 % Capture the plot as an image 
 frame       = getframe(hfig); 
@@ -7,9 +9,9 @@ im          = frame2im(frame);
 [imind,cm]  = rgb2ind(im,256); 
 % Write to the GIF File 
 if i_frame == 1 
-  imwrite(imind, cm, save_gif_filename, 'gif', 'Loopcount', inf, 'DelayTime', framerate ); 
+  imwrite(imind, cm, save_gif_filename, 'gif', 'Loopcount', inf, 'DelayTime', 1/framerate ); 
 else 
-  imwrite(imind, cm, save_gif_filename, 'gif', 'WriteMode', 'append', 'DelayTime', framerate ); 
+  imwrite(imind, cm, save_gif_filename, 'gif', 'WriteMode', 'append', 'DelayTime', 1/framerate ); 
 end 
 
 end
